2251f165644a7305ceaad63bae21a2bf19ac4270
eva.olesa <eva.olesa@securitasdirect.es>
2022-11-08 15:08:45 +0100

    deleted default files

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,18 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
- */
+import React from 'react';
+import { ProductContextProvider } from './src/context/ProductContext';
+import { CartContextProvider } from './src/context/CartContext';
+import { GlobalStyle } from './src/components/globalStyles';
 
-// You can delete this file if you're not using it
+export const wrapRootElement = ({ element }) => (
+  <ProductContextProvider>
+    <CartContextProvider>{element}</CartContextProvider>
+  </ProductContextProvider>
+);
+
+export const wrapPageElement = ({ element }) => (
+  <>
+    <GlobalStyle />
+    {element}
+  </>
+);
+
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,12 +1,17 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-ssr/
- */
+import React from 'react';
+import { ProductContextProvider } from './src/context/ProductContext';
+import { CartContextProvider } from './src/context/CartContext';
+import { GlobalStyle } from './src/components/globalStyles';
 
-/**
- * @type {import('gatsby').GatsbySSR['onRenderBody']}
- */
-exports.onRenderBody = ({ setHtmlAttributes }) => {
-  setHtmlAttributes({ lang: `en` })
-}
+export const wrapRootElement = ({ element }) => (
+  <ProductContextProvider>
+    <CartContextProvider>{element}</CartContextProvider>
+  </ProductContextProvider>
+);
+
+export const wrapPageElement = ({ element }) => (
+  <>
+    <GlobalStyle />
+    {element}
+  </>
+);
